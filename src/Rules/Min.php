5178ba70a0a73224ffaada338b463f39bb4f9e6b
Raoul van Wijk <72564782+RaoulvanWijk@users.noreply.github.com>
2023-02-25 17:21:37 +0100

    Added min rule

---
+++ src/Rules/Min.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Raoul\Validator\Rules;
+
+class Min implements \Raoul\Validator\Contracts\RuleInterface
+{
+  protected $field;
+  protected $value;
+  protected $params;
+  protected $data;
+  public function __construct($field, $value, $params, $data)
+  {
+    $this->field = $field;
+    $this->value = $value;
+    $this->params = $params;
+    $this->data = $data;
+  }
+  public function validate(): bool
+  {
+    return strlen($this->value) >= $this->params[0];
+  }
+
+  public function getMessage(): string
+  {
+    return "The {$this->field} field must be at least {$this->params[0]} characters";
+  }
+}
